a664ee7159a1867e3391f38213734a454c741f91
kZhel <zheleznikova.katya@gmail.com>
2022-11-07 16:25:50 +0300

    Initial Commit

---
+++ Rick&Morty/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Rick&Morty
+//
+//  Created by Katya on 07.11.2022.
+//  Copyright © 2022 Katya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
