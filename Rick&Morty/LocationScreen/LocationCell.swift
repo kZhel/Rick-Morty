195d7a2a5c362bde2126ecb11d737aa8d26077da
Zheleznikova Ekaterina <zheleznikova.katya@gmail.com>
2022-11-23 22:52:55 +0300

    Skopin fix labels

--- Rick&Morty/LocationScreen/LocationCell.swift
+++ Rick&Morty/LocationScreen/LocationCell.swift
@@ -27,25 +27,25 @@ class LocationCell:UICollectionViewCell {
         labelName.text = "Earth (C-137)"
         imageView.addSubview(labelName)
         labelName.widthAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
-        labelName.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1.6).isActive = true
+        labelName.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 0.25).isActive = true
         labelName.textAlignment = .center
-        labelName.bottomAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
+        labelName.topAnchor.constraint(equalTo: imageView.topAnchor).isActive = true
         
         var labelNumS = UILabel()
         labelNumS.translatesAutoresizingMaskIntoConstraints = false
-        labelNumS.backgroundColor = UIColor(white: 1, alpha: 0)
+        labelNumS.backgroundColor = UIColor(white: 1, alpha: 0.4)
         labelNumS.font = UIFont.systemFont(ofSize: 9)
         labelNumS.textColor = .white
         labelNumS.text="Type"
         imageView.addSubview(labelNumS)
         labelNumS.widthAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
-        labelNumS.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1.6).isActive = true
+        labelNumS.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 0.1).isActive = true
         labelNumS.textAlignment = .center
-        labelNumS.bottomAnchor.constraint(equalTo: labelName.bottomAnchor, constant: 25).isActive = true
+        labelNumS.topAnchor.constraint(equalTo: labelName.bottomAnchor).isActive = true
         
         var labelNumB = UILabel()
         labelNumB.translatesAutoresizingMaskIntoConstraints = false
-        labelNumB.backgroundColor = UIColor(white: 1, alpha: 0)
+        labelNumB.backgroundColor = UIColor(white: 1, alpha: 0.4)
         labelNumB.font = UIFont.boldSystemFont(ofSize: 17)
         labelNumB.textColor = .white
         labelNumB.lineBreakMode = NSLineBreakMode.byWordWrapping
@@ -53,26 +53,26 @@ class LocationCell:UICollectionViewCell {
         labelNumB.text = "Planet"
         imageView.addSubview(labelNumB)
         labelNumB.widthAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
-        labelNumB.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1.6).isActive = true
+        labelNumB.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 0.25).isActive = true
         labelNumB.textAlignment = .center
-        labelNumB.bottomAnchor.constraint(equalTo: labelNumS.bottomAnchor, constant: 25).isActive = true
+        labelNumB.topAnchor.constraint(equalTo: labelNumS.bottomAnchor).isActive = true
         
         var labelDateS = UILabel()
         labelDateS.translatesAutoresizingMaskIntoConstraints = false
-        labelDateS.backgroundColor = UIColor(white: 1, alpha: 0)
+        labelDateS.backgroundColor = UIColor(white: 1, alpha: 0.4)
         labelDateS.font = UIFont.systemFont(ofSize: 9)
         labelDateS.textColor = .white
         labelDateS.text="Dimension"
         imageView.addSubview(labelDateS)
         labelDateS.widthAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
-        labelDateS.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1.6).isActive = true
+        labelDateS.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 0.1).isActive = true
         labelDateS.textAlignment = .center
-        labelDateS.bottomAnchor.constraint(equalTo: labelNumB.bottomAnchor, constant: 25).isActive = true
+        labelDateS.topAnchor.constraint(equalTo: labelNumB.bottomAnchor).isActive = true
         
         
         var labelDateB = UILabel()
         labelDateB.translatesAutoresizingMaskIntoConstraints = false
-        labelDateB.backgroundColor = UIColor(white: 1, alpha: 0)
+        labelDateB.backgroundColor = UIColor(white: 1, alpha: 0.4)
         labelDateB.font = UIFont.boldSystemFont(ofSize: 17)
         labelDateB.textColor = .white
         labelDateB.lineBreakMode = NSLineBreakMode.byWordWrapping
@@ -80,9 +80,9 @@ class LocationCell:UICollectionViewCell {
         labelDateB.text = "Dimension C-137"
         imageView.addSubview(labelDateB)
         labelDateB.widthAnchor.constraint(equalTo: imageView.widthAnchor).isActive = true
-        labelDateB.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 1.6).isActive = true
+        labelDateB.heightAnchor.constraint(equalTo: imageView.heightAnchor, multiplier: 0.3).isActive = true
         labelDateB.textAlignment = .center
-        labelDateB.bottomAnchor.constraint(equalTo: labelDateS.bottomAnchor, constant: 25).isActive = true
+        labelDateB.bottomAnchor.constraint(equalTo: imageView.bottomAnchor).isActive = true
        
         
         return imageView
