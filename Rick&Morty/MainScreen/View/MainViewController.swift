b3cd78eb6cb626c6f224cd559eb2d90d217c3a2b
Zheleznikova Ekaterina <zheleznikova.katya@gmail.com>
2022-11-23 18:15:48 +0300

    Molokanova main screen

--- Rick&Morty/MainScreen/View/MainViewController.swift
+++ Rick&Morty/MainScreen/View/MainViewController.swift
@@ -6,45 +6,150 @@
 //  Copyright © 2022 Katya. All rights reserved.
 //
 
+
+
 import UIKit
 
-class MainViewController: UIViewController{
-    private let testButton: UIButton = {
-        let button = UIButton()
-        button.setTitle("Characters", for: .normal)
-        button.setTitleColor(.black, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
+class MainViewController: UIViewController {
+
+    // Dependencies
+
+    // UI
+    private let charactersButton = MainScreenButton(with: .characters)
+    private let locationsButton = MainScreenButton(with: .locations)
+    private let episodesButton = MainScreenButton(with: .episodes)
+
+    // MARK: - Init
+
+//    init(presenter: IMainScreenPresenter) {
+//        self.presenter = presenter
+//        super.init(nibName: nil, bundle: nil)
+//    }
+
+//    required init?(coder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+
+    // MARK: - Lyfecycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
-        setupButton()
-        setConstraints()
-        testButton.addTarget(self, action: #selector(testButtonTapped), for: .touchUpInside)
+        view.backgroundColor = .mainThemeColor
     }
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-              self.navigationController?.setNavigationBarHidden(true, animated: animated) // <<
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        setUpButton(button: charactersButton)
+        setUpButton(button: locationsButton)
+        setUpButton(button: episodesButton)
+        
     }
     override func viewWillDisappear(_ animated: Bool) {
          self.navigationController?.setNavigationBarHidden(false, animated: animated); // <<
          super.viewWillDisappear(animated)
     }
-    
-    @objc private func testButtonTapped(){
-        let charachterVC = CharactersViewController()
-        navigationController?.pushViewController(charachterVC, animated: true)
-    }
-    private func setupButton(){
-        view.addSubview(testButton)
+}
+
+// MARK: - MainScreenButtonDelegate
+
+extension MainViewController: MainScreenButtonDelegate {
+
+    func didTapMainScreenButton(with type: MainScreenButtonType) {
+        switch type {
+        case .characters:
+            let charachterVC = CharactersViewController()
+            navigationController?.pushViewController(charachterVC, animated: true)
+        case .episodes:
+            let charachterVC = CharactersViewController()
+            navigationController?.pushViewController(charachterVC, animated: true)
+        case .locations:
+            let charachterVC = CharactersViewController()
+        }
         
     }
-    private func setConstraints(){
-        NSLayoutConstraint.activate([testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-                                     testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
-                                     testButton.heightAnchor.constraint(equalToConstant: 50),
-                                     testButton.widthAnchor.constraint(equalToConstant: 150)
-        ])
+}
+
+// MARK: - Private functions
+
+private extension MainViewController {
+
+    func setUpButton(button: MainScreenButton) {
+        button.layer.cornerRadius = 20
+        button.layer.masksToBounds = true
+        button.delegate = self
+        view.addSubview(button)
+        let label = UILabel()
+        label.textColor = .black
+        label.font = UIFont.boldSystemFont(ofSize: 30)
+        label.backgroundColor = .none
+        label.backgroundColor = UIColor.white.withAlphaComponent(0.5)
+        button.addSubview(label)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.widthAnchor.constraint(equalTo: button.widthAnchor).isActive = true
+        label.heightAnchor.constraint(equalTo: button.heightAnchor).isActive = true
+        label.bottomAnchor.constraint(equalTo: button.bottomAnchor).isActive = true
+        
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        button.widthAnchor.constraint(equalToConstant: view.frame.width * 0.9).isActive = true
+        button.heightAnchor.constraint(equalToConstant: view.frame.height * 0.25).isActive = true
+        
+
+        switch button.type {
+        case .characters:
+            label.text = "Characters"
+            button.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        case .locations:
+            label.text = "Locations"
+            button.topAnchor.constraint(equalTo: charactersButton.bottomAnchor, constant: 30).isActive = true
+        case .episodes:
+            label.text = "Episodes"
+            button.topAnchor.constraint(equalTo: locationsButton.bottomAnchor, constant: 30).isActive = true
+        }
     }
 }
+
+//import UIKit
+//
+//class MainViewController: UIViewController{
+//    private let testButton: UIButton = {
+//        let button = UIButton()
+//        button.setTitle("Characters", for: .normal)
+//        button.setTitleColor(.black, for: .normal)
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        return button
+//    }()
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//        view.backgroundColor = .yellow
+//        setupButton()
+//        setConstraints()
+//        testButton.addTarget(self, action: #selector(testButtonTapped), for: .touchUpInside)
+//    }
+//    override func viewWillAppear(_ animated: Bool) {
+//        super.viewWillAppear(animated)
+//              self.navigationController?.setNavigationBarHidden(true, animated: animated) // <<
+//    }
+//    override func viewWillDisappear(_ animated: Bool) {
+//         self.navigationController?.setNavigationBarHidden(false, animated: animated); // <<
+//         super.viewWillDisappear(animated)
+//    }
+//
+//    @objc private func testButtonTapped(){
+//        let charachterVC = CharactersViewController()
+//        navigationController?.pushViewController(charachterVC, animated: true)
+//    }
+//    private func setupButton(){
+//        view.addSubview(testButton)
+//
+//    }
+//    private func setConstraints(){
+//        NSLayoutConstraint.activate([testButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+//                                     testButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+//                                     testButton.heightAnchor.constraint(equalToConstant: 50),
+//                                     testButton.widthAnchor.constraint(equalToConstant: 150)
+//        ])
+//    }
+//}
