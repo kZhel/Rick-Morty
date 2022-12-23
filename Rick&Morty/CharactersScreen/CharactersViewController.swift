1059a622cbbfb3581ff15800216224b9c5138af0
Zheleznikova Ekaterina <zheleznikova.katya@gmail.com>
2022-12-24 00:19:08 +0300

    add search and filter

--- Rick&Morty/CharactersScreen/CharactersViewController.swift
+++ Rick&Morty/CharactersScreen/CharactersViewController.swift
@@ -5,10 +5,11 @@ protocol CharactersViewProtocol {
     func success()
     func failure(error: Error)
 }
-final class CharactersViewController: UIViewController {
+final class CharactersViewController: UIViewController, UISearchBarDelegate {
     var myCollectionView:UICollectionView?
     let rmClient = RMClient()
     var charactersArr: [CharacterModel]?
+    var charactersActualData: [CharacterModel]?
     private var photos = #imageLiteral(resourceName: "tumblr_n45cr8dmj61ty0km0o7_1280")
     override func viewDidLoad() {
         let view = UIView()
@@ -49,8 +50,25 @@ final class CharactersViewController: UIViewController {
         navigationItem.searchController = searchController
         navigationController?.navigationBar.backgroundColor = .mainThemeColor
         navigationItem.hidesSearchBarWhenScrolling = false
+        searchController.searchBar.delegate = self
+    }
+    
+    func searchBarTextDidBeginEditing(_ searchBar: UISearchBar) {
+        charactersActualData = charactersArr
         
     }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        charactersArr = charactersActualData?.filter { $0.name.lowercased().contains(searchText.lowercased())}
+        myCollectionView?.reloadData()
+    }
+    
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        charactersArr = charactersActualData
+        myCollectionView?.reloadData()
+        charactersActualData = nil
+    }
+    
     private func setupCollectionView(){
         myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: createLayout())
         myCollectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
@@ -76,11 +94,29 @@ final class CharactersViewController: UIViewController {
     }
 
     @objc private func filterButtonTapped(){
-        let swiftUIView = ContentView() // swiftUIView is View
+        let viewToFilter = self;
+        let swiftUIView = FiltrationView.getInstance(charViewToFilter: viewToFilter) // swiftUIView is View
         let viewCtrl = UIHostingController(rootView: swiftUIView)
         navigationController?.pushViewController(viewCtrl, animated: true)
         
+    }
+    
+    public func setFiltration(status: String, gender: String) { //todo: chto takoe completion hz (i kak menyat raskladku na ebanom apple?)
+        let characters = self.rmClient.character();
+        let filter = characters.createCharacterFilter(name: nil, status: Status(rawValue: status), species: nil, type: nil, gender: Gender(rawValue: gender))
         
+        characters.getCharactersByFilter(filter: filter) {[weak self] result in
+            guard let self = self else {return}
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let characters):
+                    self.charactersArr = characters
+                    self.success()
+                case.failure(let error):
+                    self.failure(error: error)
+                }
+            }
+        }
     }
     
 }
@@ -100,6 +136,7 @@ extension UIViewController{
         return menuBarButton
     }
 }
+
 extension CharactersViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return charactersArr?.count ?? 0
--- Rick&Morty/CharactersScreen/FilterScreenView.swift
+++ Rick&Morty/CharactersScreen/FilterScreenView.swift
@@ -8,17 +8,28 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct FiltrationView: View {
+    @Environment(\.presentationMode) private var presentationMode: Binding<PresentationMode>
+    
+    
+    var charView: CharactersViewController? = nil;
+    
+    public static func getInstance(charViewToFilter: CharactersViewController) -> FiltrationView {
+        var retView: FiltrationView = FiltrationView();
+        retView.charView = charViewToFilter;
+        return retView
+    }
     
     @State var status : String = ""
     @State var gender : String = ""
     enum Gender : String {
-        case Female,Male,Genderless,Unknown
+        case female,male,genderless,unknown
     }
     
     enum Status : String{
-        case Alive, Dead, Unknown
+        case alive, dead, unknown
     }
+    
     var colorNotPicked : Color = Color.white
     var colorPicked : Color = Color.green
     var backgroundColor : Color = Color(red: 0.15, green: 0.15, blue: 0.19, opacity: 1)
@@ -40,13 +51,13 @@ struct ContentView: View {
                 }
                 
                 HStack{
-                    RadioButton(radioValue: self.$status, label: Status.Alive.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
+                    RadioButton(radioValue: self.$status, label: "Alive",value:Status.alive.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
                 }
                 HStack{
-                    RadioButton(radioValue: self.$status, label: Status.Dead.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
+                    RadioButton(radioValue: self.$status, label: "Dead",value: Status.dead.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
                 }
                 HStack{
-                    RadioButton(radioValue: self.$status, label: Status.Unknown.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
+                    RadioButton(radioValue: self.$status, label: "Unknown",value:Status.unknown.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
                 }
                                 
                 HStack{
@@ -61,16 +72,16 @@ struct ContentView: View {
                 }
                 
                 HStack{
-                    RadioButton(radioValue: self.$gender, label: Gender.Female.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
+                    RadioButton(radioValue: self.$gender, label: "Female", value: Gender.female.rawValue,colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
                 }
                 HStack{
-                    RadioButton(radioValue: self.$gender, label: Gender.Male.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
+                    RadioButton(radioValue: self.$gender, label: "Male", value: Gender.male.rawValue,colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
                 }
                 HStack{
-                    RadioButton(radioValue: self.$gender, label: Gender.Genderless.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
+                    RadioButton(radioValue: self.$gender, label: "Genderless",value: Gender.genderless.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
                 }
                 HStack{
-                    RadioButton(radioValue: self.$gender, label: Gender.Unknown.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
+                    RadioButton(radioValue: self.$gender, label: "Unknown",value: Gender.unknown.rawValue, colorPicked: self.colorPicked, colorNotPicked: self.colorNotPicked)
                 }
 
                 
@@ -90,45 +101,51 @@ struct ContentView: View {
     func onResetStatusClick() {
         //todo: reset radioButtons and status
         self.status = ""
+        charView?.setFiltration(status: self.status, gender: self.gender)
     }
     
     func onResetGenderClick() {
         //todo: reset gender and radios
         self.gender = ""
+        charView?.setFiltration(status: self.status, gender: self.gender)
     }
     
     func applyFilters() {
         //todo: well...apply filters, ig
+            charView?.setFiltration(status: self.status, gender: self.gender)
+            
+            //todo: close filtration window on applyFilters, bc we don't need it anymore (and show charactersViewController)
+            self.presentationMode.wrappedValue.dismiss()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        FiltrationView()
     }
 }
 
 struct RadioButton : View {
     @Binding var radioValue : String
     var label: String
+    var value: String
     var colorPicked: Color
     var colorNotPicked: Color
     
-    
     var body: some View {
-        Button(action:{self.radioValue = self.label}) {
+        Button(action:{self.radioValue = self.value}) {
             Text("   ")
                 .frame(width:20,height:20)
                 .foregroundColor(Color.black)
-                .background(radioValue == label ? colorPicked:nil)
+                .background(radioValue == value ? colorPicked:nil)
                 .cornerRadius(100)
                 .padding()
                 .overlay(RoundedRectangle(cornerRadius: 100)
-                    .stroke(radioValue == label ? colorPicked:colorNotPicked,lineWidth: 6)
+                    .stroke(radioValue == value ? colorPicked:colorNotPicked,lineWidth: 6)
                     .scaleEffect(0.5))
             Text(label).frame(maxWidth: .infinity,alignment: .leading)    .frame(maxWidth: .infinity,alignment: .leading)
             .font(Font.headline.weight(.bold))
-            .foregroundColor(radioValue == label ? colorPicked:colorNotPicked)
+            .foregroundColor(radioValue == value ? colorPicked:colorNotPicked)
         }
     }
 }
