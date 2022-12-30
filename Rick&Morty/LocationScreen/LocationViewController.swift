ab320d224b19a5ded282a036eff8c2cc3f5b09cb
Zheleznikova Ekaterina <zheleznikova.katya@gmail.com>
2022-12-30 09:23:45 +0300

    add location screen

--- Rick&Morty/LocationScreen/LocationViewController.swift
+++ Rick&Morty/LocationScreen/LocationViewController.swift
@@ -2,13 +2,30 @@ import UIKit
 import SwiftUI
 final class LocationViewController: UIViewController {
     var myCollectionView:UICollectionView?
+    let rmClient = RMClient()
+    var locationArr: [LocationModel]?
     override func viewDidLoad() {
         let view = UIView()
         super.viewDidLoad()
         view.backgroundColor = .mainThemeColor
+        getLocation()
         setupNavigationBar()
         setupCollectionView()
 
+    }
+    func getLocation(){
+        rmClient.location().getAllLocations() {[weak self] result in
+            guard let self = self else {return}
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let location):
+                    self.locationArr = location
+                    self.success()
+                case.failure(let error):
+                    self.failure(error: error)
+                }
+            }
+        }
     }
     private func setupNavigationBar(){
         createCustomNavigationBar()
@@ -22,7 +39,7 @@ final class LocationViewController: UIViewController {
     private func setupCollectionView(){
         myCollectionView = UICollectionView(frame: self.view.frame, collectionViewLayout: createLayout())
         myCollectionView?.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "MyCell")
-        myCollectionView?.register(LocationCell.self, forCellWithReuseIdentifier: LocationCell.reuseId)
+        myCollectionView?.register(LocationCustomCell.self, forCellWithReuseIdentifier: LocationCustomCell.reuseId)
         myCollectionView?.backgroundColor = .mainThemeColor
         myCollectionView?.dataSource = self
         myCollectionView?.delegate = self
@@ -47,15 +64,25 @@ final class LocationViewController: UIViewController {
 
  extension LocationViewController: UICollectionViewDataSource, UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 12
+        return locationArr?.count ?? 0
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: LocationCell.reuseId, for: indexPath) as! LocationCell
-        myCell.photoImage
+        let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: LocationCustomCell.reuseId, for: indexPath) as! LocationCustomCell
+        myCell.photoImage(name: locationArr?[indexPath.row].name ?? "", type: locationArr?[indexPath.row].type ?? "", dimention: locationArr?[indexPath.row].dimension ?? "")
         myCell.layer.cornerRadius = 6
         return myCell
     }
 }
 
-
+extension LocationViewController: CharactersViewProtocol{
+    func success() {
+        myCollectionView?.reloadData()
+    }
+    
+    func failure(error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    
+}
